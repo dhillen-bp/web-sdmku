9e8455e4dba6363e2878ce90ada24aec561521bc
dhillenbp179 <dhillen.mutu@gmail.com>
2024-03-03 07:06:40 +0700

    Fix Vite Config for Admin Template

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: ['resources/css/app.css', 'resources/js/app.js','resources/admin/css/style.css', 'resources/admin/css/satoshi.css', 'resources/admin/js/index.js'],
             refresh: true,
         }),
     ],
